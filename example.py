9a5bd4f40ea72a728da27949753f1095ca4a529b
Benjamin Hou <>
2018-03-27 16:22:33 +0100

    formatting

--- example.py
+++ example.py
@@ -2,36 +2,33 @@ from se3_geodesic_loss import SE3GeodesicLoss
 import tensorflow as tf
 import numpy as np
 
-## init SE3GeodesicLoss class with inner_product_mat_at_identity weights
-loss = SE3GeodesicLoss(np.array([1,1,1,1,1,1]))
+# init SE3GeodesicLoss class with inner_product_mat_at_identity weights
+loss = SE3GeodesicLoss(np.array([1, 1, 1, 1, 1, 1]))
 
-## init variables
+# init variables
 init = tf.global_variables_initializer()
 
 with tf.Session() as sess:
 
-	## Create random input variables 
-    y_pred = tf.random_uniform([20,6])
-    y_true = tf.random_uniform([20,6])
+    # Create random input variables
+    y_pred = tf.random_uniform([20, 6])
+    y_true = tf.random_uniform([20, 6])
 
-	## Create deterministic input variables 
-    #y_pred = tf.constant([1., 2., 3., 4., 5., 6.], shape=[20,6])
-    #y_true = tf.constant([3., 4., 5., 6., 7., 8.], shape=[20,6])
+    # Create deterministic input variables
+    # y_pred = tf.constant([1., 2., 3., 4., 5., 6.], shape=[20,6])
+    # y_true = tf.constant([3., 4., 5., 6., 7., 8.], shape=[20,6])
 
-    ## Create graph
-    dist = loss.geodesic_loss(y_pred,y_true)
+    # Create graph
+    dist = loss.geodesic_loss(y_pred, y_true)
 
-    ## Init session
+    # Init session
     sess.run(init)
 
-    ## Compute forward loss
+    # Compute forward loss
     print sess.run(dist)
-    #print dist.eval()
+    # print dist.eval()
 
-    ## Compute backward gradients
-    print sess.run(tf.gradients(dist, [y_pred,y_true]))
-    #print tf.gradients(dist, [y_pred,y_true])[0].eval() # grad w.r.t. y_pred
-    #print tf.gradients(dist, [y_pred,y_true])[1].eval() # grad w.r.t. y_true
-
-
-
+    # Compute backward gradients
+    print sess.run(tf.gradients(dist, [y_pred, y_true]))
+    # print tf.gradients(dist, [y_pred,y_true])[0].eval() # grad w.r.t. y_pred
+    # print tf.gradients(dist, [y_pred,y_true])[1].eval() # grad w.r.t. y_true
--- se3_geodesic_loss.py
+++ se3_geodesic_loss.py
@@ -8,8 +8,8 @@
 # copies of the Software, and to permit persons to whom the Software is
 # furnished to do so, subject to the following conditions:
 #
-# The above copyright notice and this permission notice shall be included in all
-# copies or substantial portions of the Software.
+# The above copyright notice and this permission notice shall be included in
+# all copies or substantial portions of the Software.
 #
 # THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
 # IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
@@ -24,8 +24,8 @@ import numpy as np
 import tensorflow as tf
 from tensorflow.python.framework import ops
 
-import sys
-sys.path.append('/vol/medic01/users/bh1511/_build/geomstats-master/')
+# import sys
+# sys.path.append('/vol/medic01/users/bh1511/_build/geomstats-master/')
 from geomstats.special_euclidean_group import SpecialEuclideanGroup
 
 
@@ -34,19 +34,20 @@ class SE3GeodesicLoss(object):
     def __init__(self, w, op_name='GeodesicDistance'):
         super(SE3GeodesicLoss, self).__init__()
 
-        assert w.shape != 6 , 'Weight vector must be of shape 1x6'
+        assert w.shape != 6, 'Weight vector must be of shape 1x6'
 
         self.op_name = op_name
         self.SE3_GROUP = SpecialEuclideanGroup(3)
         self.w = w
-        self.SE3_GROUP.left_canonical_metric.inner_product_mat_at_identity = np.eye(6) * self.w
+        self.SE3_GROUP.left_canonical_metric.inner_product_mat_at_identity = \
+            np.eye(6) * self.w
         self.metric = self.SE3_GROUP.left_canonical_metric
 
     # Python Custom Op Tensorflow Wrapper
     def py_func(self, func, inp, Tout, stateful=True, name=None, grad=None):
         """
         PyFunc defined as given by Tensorflow
-    
+
         :param func:        Custom Function
         :param inp:         Function Inputs
         :param Tout:        Ouput Type of out Custom Function
@@ -55,88 +56,84 @@ class SE3GeodesicLoss(object):
         :param grad:        Custom Gradient Function
         :return:
         """
-        # Generate Random Gradient name in order to avoid conflicts with inbuilt names
+        # Generate Random Gradient name to avoid conflicts with inbuilt names
         rnd_name = 'PyFuncGrad' + str(np.random.randint(0, 2**32-1))
-    
+
         # Register Tensorflow Gradient
         tf.RegisterGradient(rnd_name)(grad)
-    
+
         # Get current graph
         g = tf.get_default_graph()
-    
+
         # Add gradient override map
-        with g.gradient_override_map({'PyFunc': rnd_name, 'PyFuncStateless': rnd_name}):
+        with g.gradient_override_map({
+                'PyFunc': rnd_name,
+                'PyFuncStateless': rnd_name}):
             return tf.py_func(func, inp, Tout, stateful=stateful, name=name)
-    
-    
-    
-    # Python Custom Op  
+
+    # Python Custom Op
     def geodesic_loss(self, y_pred, y_true, name=None):
         """
         Custom Function which defines pyfunc and gradient override
         :param x:       y_pred - predicted se(3) pose
         :param y:       y_true - ground truth se(3) pose
         :param name:    Function name
-        :return:        dist - geodesic distance between predicted pose and ground truth pose
+        :return:        dist - geodesic distance between y_pred and y_true
         """
         with ops.name_scope(name, self.op_name, [y_pred, y_true]) as name:
             """
-            Our pyfunc accepts 2 input parameters and returns 2 outputs
+            Our pyfunc op accepts 2 input parameters and returns 1 outputs
             Input Parameters:   y_pred, y_true
-            Output Parameters:  geodesic distance, geodesic distance w.r.t. y_pred
+            Output Parameters:  geodesic distance
             """
-            dist , grad = self.py_func(self.riemannian_dist_grad,
-                                       [y_pred, y_true],
-                                       [tf.float32, tf.float32],
-                                       name=name,
-                                       grad=self.riemannian_grad_op)
+            dist, grad = self.py_func(self.riemannian_dist_grad,
+                                      [y_pred, y_true],
+                                      [tf.float32, tf.float32],
+                                      name=name,
+                                      grad=self.riemannian_grad_op)
             return dist
-    
-    
-    # Geodesic Loss Core Function 
+
+    # Geodesic Loss Core Function
     def riemannian_dist_grad(self, y_pred, y_true):
         """
-        Geodesic Loss Core Function 
-    
+        Geodesic Loss Core Function
+
         :param y_pred: y_pred
         :param y_true: y_true
         :return: dist, grad
         """
-        # Geodesic Distance 
-        dist = np.squeeze(self.metric.squared_dist(y_pred, y_true)).astype('float32')
-    
+        # Geodesic Distance
+        dist = np.squeeze(self.metric.squared_dist(y_pred, y_true))\
+            .astype('float32')
+
         # d/dx (Geodesic Distance)
-        tangent_vec = np.squeeze(self.metric.log(base_point=y_pred, point=y_true)).astype('float32')
-        
+        tangent_vec = self.metric.log(base_point=y_pred, point=y_true)
+        tangent_vec = np.squeeze(tangent_vec).astype('float32')
+
         grad_point = - 2. * tangent_vec
-    
-        inner_prod_mat = np.squeeze(self.metric.inner_product_matrix(y_pred)).astype('float32')
-    
-        grad_point = np.repeat(np.expand_dims(grad_point,axis=1),6,axis=1)
-        grad = np.sum(np.multiply(inner_prod_mat,grad_point),axis=2)
-    
+
+        inner_prod_mat = self.metric.inner_product_matrix(y_pred)
+        inner_prod_mat = np.squeeze(inner_prod_mat).astype('float32')
+
+        grad_point = np.repeat(np.expand_dims(grad_point, axis=1), 6, axis=1)
+        grad = np.sum(np.multiply(inner_prod_mat, grad_point), axis=2)
+
         sqrt_w = np.sqrt(self.w).astype('float32')
-        grad = np.multiply(grad,sqrt_w)
-    
+        grad = np.multiply(grad, sqrt_w)
+
         return np.sum(dist), grad
-    
-    
+
     # Geodesic Loss Gradient Function
     def riemannian_grad_op(self, op, grads, grad_glob):
         """
         Geodesic Loss Gradient Function
-    
-        :param op:      Operation - operation.inputs = [y_pred, y_true], operation.outputs=[dist, grad]
-        :param grads:   Gradients for equation prime
-        :param grad_glob: - No real use of it, but the gradient function parameter size should match op.inputs
-        :return: grads * d/d_y_pred , vector of ones
+
+        :param op:          Operation - operation.inputs  = [y_pred, y_true],
+                                        operation.outputs = [dist, grad]
+        :param grads:       Gradients for equation prime
+        :param grad_glob:   No real use of it, but the gradient function
+                            parameter size should match op.inputs
+        :return:            grads * d/d_y_pred , vector of ones
         """
-        # Only gradient w.r.t. y_pred is returned. 
-        return grads * op.outputs[1] , tf.ones_like(op.outputs[1]) 
-    
-
-
-
-
-
-
+        # Only gradient w.r.t. y_pred is returned.
+        return grads * op.outputs[1], tf.ones_like(op.outputs[1])
