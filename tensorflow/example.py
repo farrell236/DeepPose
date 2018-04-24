7819d61ee40d4fcfe5cffc6e905febbbe2c055fb
Benjamin Hou <>
2018-04-24 12:22:44 +0100

    code clean up tensorflow/example.py

--- tensorflow/example.py
+++ tensorflow/example.py
@@ -1,34 +1,43 @@
-from se3_geodesic_loss import SE3GeodesicLoss
+"""
+Example usage of se3_geodesic_loss for TensorFlow Framework
+
+Input tensors:
+    y_pred : predicted se3 pose (shape = [Nx6])
+    y_true : ground truth se3 pose (shape = [Nx6])
+
+Output tensor:
+    loss   : total squared geodesic loss of all N examples
+
+N.B. Gradient tensor (shape = [Nx6]) is only calculated w.r.t. y_pred.
+Gradient w.r.t. y_true is a vector of ones.
+"""
+
 import tensorflow as tf
 import numpy as np
 
-# init SE3GeodesicLoss class with inner_product_mat_at_identity weights
-loss = SE3GeodesicLoss(np.array([1, 1, 1, 1, 1, 1]))
+from se3_geodesic_loss import SE3GeodesicLoss
 
-# init variables
-init = tf.global_variables_initializer()
+
+N_SAMPLES = 20
+SE3_DIM = 6
+
+loss = SE3GeodesicLoss(np.ones(SE3_DIM))
 
 with tf.Session() as sess:
 
     # Create random input variables
-    y_pred = tf.random_uniform([20, 6])
-    y_true = tf.random_uniform([20, 6])
+    # y_pred = tf.random_normal([N_SAMPLES, SE3_DIM])
+    # y_true = tf.random_normal([N_SAMPLES, SE3_DIM])
 
     # Create deterministic input variables
-    # y_pred = tf.constant([1., 2., 3., 4., 5., 6.], shape=[20,6])
-    # y_true = tf.constant([3., 4., 5., 6., 7., 8.], shape=[20,6])
+    y_pred = tf.constant([1., 2., 3., 4., 5., 6.], shape=[N_SAMPLES, SE3_DIM])
+    y_true = tf.constant([3., 4., 5., 6., 7., 8.], shape=[N_SAMPLES, SE3_DIM])
 
     # Create graph
-    dist = loss.geodesic_loss(y_pred, y_true)
-
-    # Init session
-    sess.run(init)
+    geodesic_loss = loss.geodesic_loss(y_pred, y_true)
 
     # Compute forward loss
-    print sess.run(dist)
-    # print dist.eval()
+    print sess.run(geodesic_loss)
 
     # Compute backward gradients
-    print sess.run(tf.gradients(dist, [y_pred, y_true]))
-    # print tf.gradients(dist, [y_pred,y_true])[0].eval() # grad w.r.t. y_pred
-    # print tf.gradients(dist, [y_pred,y_true])[1].eval() # grad w.r.t. y_true
+    print sess.run(tf.gradients(geodesic_loss, [y_pred, y_true]))
